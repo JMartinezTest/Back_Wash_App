9180dea1d08236362f0b1efcc4db63139e91ef8b
JMartinezTest <jorgemariomartinez16@protonmail.com>
2026-04-22 08:00:56 -0500

    update

--- San_felipe_back/San_felipe_back/San_felipe_back/src/main/java/com/proyecto/san_felipe/Repository/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto.san_felipe.Repository;
-
-import com.proyecto.san_felipe.entities.Car;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CarRepository extends MongoRepository<Car, String> {
-    Car findByLicencePlate(String licencePlate);
-}
--- San_felipe_back/San_felipe_back/San_felipe_back/src/main/java/com/proyecto/san_felipe/security/WebConfig.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto.san_felipe.security;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WebConfig {
-
-}
